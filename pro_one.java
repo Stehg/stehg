8f6d21610d07c71dfb57c139c5978663e6d681a9
Steven <dangelo82008@hotmail.com>
2023-05-19 07:27:54 -0500

    Este es el 3 v2. Main

--- pro_one.java
+++ pro_one.java
@@ -1,7 +1,7 @@
 
 public class pro_one {
     public static void main(String[] args) {
-        String xcode = "Soy el primero.";
+        String xcode = "Soy el primero.evolucion";
     System.out.println(xcode);
 
 
--- pro_three.java
+++ pro_three.java
@@ -1,4 +1,5 @@
 public class pro_three {
-    System.out.println("Soy el tercero.")
+    public static void main(String[] args){
+    System.out.println("Soy el tercero.");
     
-}
+}}
