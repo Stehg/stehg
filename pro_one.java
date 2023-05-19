1f81de1473d2a4519976ebc3be98414e232c6925
Steven <dangelo82008@hotmail.com>
2023-05-19 12:41:45 -0500

    modificado pro_one.

--- pro_one.java
+++ pro_one.java
@@ -1,7 +1,7 @@
 
 public class pro_one {
     public static void main(String[] args) {
-        String xcode = "Soy el primero.evolucion";
+        String xcode = "Soy el primero.";
     System.out.println(xcode);
 
 
